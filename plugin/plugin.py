602b1d3490afc84f172e44523744a2dec6c1127a
ims <ims21@users.sourceforge.net>
2018-03-01 20:08:11 +0100

    plugin.py

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -1,10 +1,4 @@
-#from Plugins.Plugin import PluginDescriptor
+from Plugins.Plugin import PluginDescriptor
 
-#def main(session, service, **kwargs):
-#	from Screens.MovieSelection import playlist
-#	import ui
-#	session.open(ui.MovieManager, playlist)
-
-#def Plugins(**kwargs):
-#	return PluginDescriptor(name= _("Movie Manager..."), description=_("Movie Manager..."),
-#		where = PluginDescriptor.WHERE_MOVIELIST, needsRestart = False, fnc=main)
+def Plugins(**kwargs):
+	return PluginDescriptor()
