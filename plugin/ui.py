06b7baf85fb7f391270c69b9d568a9b06ab6e42e
ims <ims21@users.sourceforge.net>
2021-01-02 14:56:54 +0100

    2.00 - added 'Include trashcans' sub-option for 'Including subdirectories'

--- plugin/ui.py
+++ plugin/ui.py
@@ -4,8 +4,8 @@ from . import _, ngettext
 
 #
 #  Movie Manager - Plugin E2 for OpenPLi
-VERSION = "1.99"
-#  by ims (c) 2018-2020 ims@openpli.org
+VERSION = "2.00"
+#  by ims (c) 2018-2021 ims@openpli.org
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
@@ -74,6 +74,7 @@ config.moviemanager.clear_bookmarks = ConfigYesNo(default=True)
 config.moviemanager.manage_all = ConfigYesNo(default=False)
 config.moviemanager.removepkl = ConfigYesNo(default=False)
 config.moviemanager.subdirs = ConfigYesNo(default=False)
+config.moviemanager.trashcans = ConfigYesNo(default=False)
 config.moviemanager.recordings = ConfigYesNo(default=True)
 config.moviemanager.other_movies = ConfigYesNo(default=True)
 config.moviemanager.pictures = ConfigYesNo(default=False)
@@ -514,6 +515,7 @@ class MovieManager(Screen, HelpableScreen):
 		s += 0x10 if cfg.dvds.value else 0
 		s += 0x20 if cfg.manage_all.value else 0
 		s += 0x40 if cfg.recordings.value else 0
+		s += 0x80 if cfg.trashcans.value else 0
 		return s
 
 	def saveList(self):
@@ -687,10 +689,15 @@ class MovieManager(Screen, HelpableScreen):
 		def lookDirs(path):
 			paths = []
 			for path, dirs, files in os.walk(path):
-				if path.find("BDMV") == -1 and path.find("VIDEO_TS") == -1 and path.find("AUDIO_TS") == -1: # and path.find(".Trash") == -1:
-					if not path.endswith('/'):
-						path += '/'
-					paths.append(path)
+				if path.find("BDMV") != -1:
+					continue
+				if path.find("VIDEO_TS") != -1 or path.find("AUDIO_TS") != -1:
+					continue
+				if not cfg.trashcans.value and path.find(".Trash") != -1:
+					continue
+				if not path.endswith('/'):
+					path += '/'
+				paths.append(path)
 				if PKLFILE in files:
 					self.pklPaths.append(path.rstrip('/'))
 			return paths
@@ -1185,7 +1192,11 @@ class MovieManagerCfg(Screen, ConfigListScreen):
 		self.list.append(getConfigListEntry(_("Enable 'Clear bookmark...'"), cfg.clear_bookmarks, _("Enable in menu utility for delete bookmarks in menu.")))
 		self.list.append(getConfigListEntry(_("Enable 'Remove local directory setting...'"), cfg.removepkl, _("Enable item for delete local directory setting in menu. It depends on displayed directories in movielist.")))
 		self.list.append(getConfigListEntry(_("Enable 'Manage files in active bookmarks...'"), cfg.manage_all, _("Enable in menu item for manage movies in all active bookmarks as one list.") + note))
-		self.list.append(getConfigListEntry(_("Including subdirectories"), cfg.subdirs, _("If enabled, then will be used subdirectories too (it will take longer).") + note))
+		self.subdirs = _("Including subdirectories")
+		self.list.append(getConfigListEntry(self.subdirs, cfg.subdirs, _("If enabled, then will be used subdirectories too (it will take longer).") + note))
+		if cfg.subdirs.value:
+			dx = 4*" "
+			self.list.append(getConfigListEntry(dx + _("Include trashcans"), cfg.trashcans, _("Items from trashcans will be added into list too.") + note))
 		self.list.append(getConfigListEntry(_("Recordings"), cfg.recordings, _("If enabled, then will be added recordings into list.") + note))
 		self.list.append(getConfigListEntry(_("Other movie files"), cfg.other_movies, _("If enabled, then will be added other movie files into list.") + note))
 		self.list.append(getConfigListEntry(_("Audio files"), cfg.audios, _("If enabled, then will be added audio files into list.") + note))
@@ -1208,7 +1219,7 @@ class MovieManagerCfg(Screen, ConfigListScreen):
 
 	# Summary - for (LCD):
 	def changedEntry(self):
-		if self["config"].getCurrent()[0] in (self.search, self.csv_extended):
+		if self["config"].getCurrent()[0] in (self.search, self.csv_extended, self.subdirs):
 			self.loadMenu()
 		for x in self.onChangedEntry:
 			x()
